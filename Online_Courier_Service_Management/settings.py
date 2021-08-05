15cf0209818819ca27b3320fea55745c7a638898
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-05 23:41:05 +0600

    added main.py

--- Online_Courier_Service_Management/settings.py
+++ Online_Courier_Service_Management/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = 'django-insecure-k#sucg_%%b6c$c&qmer=a+n!#jh=gl@ex79s595k7bl5)y_w@e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
