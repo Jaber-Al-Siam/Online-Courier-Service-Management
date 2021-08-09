8a8ce0cb04d9f19d81d6ef09a896ad1873798966
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-09 18:59:09 +0600

    changed library from django-bootstrap5 to django-bootstrap-v5

--- Online_Courier_Service_Management/settings.py
+++ Online_Courier_Service_Management/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'crispy_forms',
-    'django_bootstrap5',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -137,7 +137,7 @@ MEDIA_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'media/'))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CRISPY_TEMPLATE_PACK = 'django_bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = 'home_page'
 
