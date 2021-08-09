41e6a69072da55fbb3bca777fefbcf8e663f4ae1
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-09 15:17:45 +0600

    added urls for password reset, signup and logout

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,20 @@
 from typing import Any, List
 from django.urls import path
 from users import views as users_views
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns: List[Any] = [
-    # path('signup/', '#', name='signup_page'),
+    path('signup/', users_views.signup, name='signup_page'),
+    path('profile/', users_views.viewOrUpdateProfile, name='profile_page'),
     path('', users_views.home, name='home_page'),
-    path('login/', views.LoginView.as_view(template_name='login.html'), name='login_page'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login_page'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logged_out.html'), name='logout_page'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='password_reset.html'), name='password_reset_page'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'), name='password_reset_done_page'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='password_reset_confirm.html'), name='password_reset_confirm_page'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'), name='password_reset_complete_page'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    return render(request, 'users/signup.html')
+
+
+def viewOrUpdateProfile(request):
+    return render(request, 'users/profile.html')
