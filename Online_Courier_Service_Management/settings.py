65c2726e52732997dd9b9d75e61729c44a1ac64b
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-12 02:15:54 +0600

    created parcels and jobs app

--- Online_Courier_Service_Management/settings.py
+++ Online_Courier_Service_Management/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'parcels.apps.ParcelsConfig',
+    'jobs.apps.JobsConfig',
     'crispy_forms',
     'bootstrap5',
 ]
