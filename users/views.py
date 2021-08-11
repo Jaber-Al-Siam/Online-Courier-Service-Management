f7991b039d05b185fe3cacbc392ce97581608b0b
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-11 14:39:54 +0600

    created user signup view

--- users/views.py
+++ users/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from users.forms import SignupForm
+from django.contrib import messages
+from django.contrib.auth import authenticate
 
 
 # Create your views here.
@@ -8,9 +12,28 @@ def home(request):
     return render(request, 'index.html')
 
 
-def signup(request):
-    return render(request, 'users/signup.html')
-
-
 def viewOrUpdateProfile(request):
     return render(request, 'users/profile.html')
+
+
+class UserCreationView(View):
+    form_class = SignupForm
+    template_name = 'users/signup.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            # username = form.cleaned_data.get('username')
+            # raw_password = form.cleaned_data.get('password1')
+            # user = authenticate(username=username, password=raw_password)
+            messages.success(request, 'Your account created successfully, please login')
+            print("form saved")
+            return redirect('login_page')
+        else:
+            form = self.form_class(request.POST)
+            return render(request, self.template_name, {'form': form})
