7ef1ea501cdd1b9344dae0f970c2f7896bbb173a
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-14 16:31:40 +0600

    created new urls

--- parcels/urls.py
+++ parcels/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import BookParcel
+from parcels import views as parcel_view
+
+app_name = 'parcels'
 
 urlpatterns = [
-    path('parcel/', BookParcel.as_view(), name='book_parcel_page')
+    path('parcels/', parcel_view.ListView.as_view(), name='parcel'),
+    path('parcels/create/', parcel_view.ParcelBook.as_view(), name='create'),
+    path('parcels/<int:pk>/', parcel_view.DetailView.as_view(), name='detail'),
 ]
