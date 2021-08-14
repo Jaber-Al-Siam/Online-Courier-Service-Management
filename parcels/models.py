a68edd1946e8e6c27a3f3e5d090687243204305e
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-14 16:29:46 +0600

    added get_absolute_url and __str__ method in Parcel model

--- parcels/models.py
+++ parcels/models.py
@@ -1,11 +1,16 @@
-import self as self
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
 
 
 class Parcel(models.Model):
     parcel_type = models.CharField(max_length=20)
-    receivers_email = models.CharField(max_length=100)
-    url = self.object.get_absolute_url()
+    receiver_email = models.CharField(max_length=100)
+    receiver_phone = models.CharField(max_length=100)
 
+    def get_absolute_url(self):
+        return reverse('parcels:detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return str(self.pk)
