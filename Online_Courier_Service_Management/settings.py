efb963871b99709fc41eba1d5d258cd0104646f8
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-14 16:32:25 +0600

    changed db to atlas cluster

--- Online_Courier_Service_Management/settings.py
+++ Online_Courier_Service_Management/settings.py
@@ -85,13 +85,12 @@ MONGODB_PASSWORD = config('MONGO_PASSWORD')
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        # 'CLIENT': {
-        #     'name': "test",
-        #     'host': 'mongodb+srv://ocsmcluster.r1xvi.mongodb.net/test?retryWrites=true&w=majority',
-        #     'username': quote_plus(MONGODB_USER),
-        #     'password': quote_plus(MONGODB_PASSWORD),
-        # },
-        'NAME': 'test',
+        'CLIENT': {
+            'name': "test",
+            'host': 'mongodb+srv://ocsmcluster.r1xvi.mongodb.net/test?retryWrites=true&w=majority',
+            'username': quote_plus(MONGODB_USER),
+            'password': quote_plus(MONGODB_PASSWORD),
+        },
     }
 }
 
