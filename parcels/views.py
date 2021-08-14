89f13d8d604c6461a30a256991c79bbd12cad43e
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-14 16:31:18 +0600

    added listview in parcels view

--- parcels/views.py
+++ parcels/views.py
@@ -1,25 +1,23 @@
-from django.core.exceptions import ImproperlyConfigured
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView
 from .models import Parcel
 
 
 # Create your views here.
 
+class ListView(ListView):
+    def get_queryset(self):
+        return Parcel.objects.all()
 
-class BookParcel(CreateView):
+
+class DetailView(DetailView):
     model = Parcel
-    fields = ['parcel_type', 'receivers_email']
 
-    def get_success_url(self):
-        """Return the URL to redirect to after processing a valid form."""
-        if self.success_url:
-            url = self.success_url.format(**self.object.__dict__)
-        else:
-            try:
-                url = self.object.get_absolute_url()
-            except AttributeError:
-                raise ImproperlyConfigured(
-                    "No URL to redirect to.  Either provide a url or define"
-                    " a get_absolute_url method on the Model.")
-        return url
+
+class ParcelBook(CreateView):
+    model = Parcel
+    fields = ['parcel_type', 'receiver_email', 'receiver_phone']
+
