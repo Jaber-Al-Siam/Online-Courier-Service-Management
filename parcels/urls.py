8a1e4e4b39ff47123734168cd948c5557850edf7
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-15 01:16:51 +0600

    refactored View classes

--- parcels/urls.py
+++ parcels/urls.py
@@ -4,7 +4,7 @@ from parcels import views as parcel_view
 app_name = 'parcels'
 
 urlpatterns = [
-    path('parcels/', parcel_view.ListView.as_view(), name='parcel'),
-    path('parcels/create/', parcel_view.ParcelBook.as_view(), name='create'),
-    path('parcels/<int:pk>/', parcel_view.DetailView.as_view(), name='detail'),
+    path('parcels/', parcel_view.ParcelListView.as_view(), name='parcel'),
+    path('parcels/create/', parcel_view.ParcelCreateView.as_view(), name='create'),
+    path('parcels/<int:pk>/', parcel_view.ParcelDetailView.as_view(), name='detail'),
 ]
--- parcels/views.py
+++ parcels/views.py
@@ -1,6 +1,4 @@
-from django.contrib import messages
-from django.shortcuts import render, redirect
-from django.views import View
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView
 from .models import Parcel
@@ -8,16 +6,15 @@ from .models import Parcel
 
 # Create your views here.
 
-class ListView(ListView):
+class ParcelListView(ListView):
     def get_queryset(self):
         return Parcel.objects.all()
 
 
-class DetailView(DetailView):
+class ParcelDetailView(DetailView):
     model = Parcel
 
 
-class ParcelBook(CreateView):
+class ParcelCreateView(CreateView):
     model = Parcel
-    fields = ['parcel_type', 'receiver_email', 'receiver_phone']
-
+    fields = ['type', 'city', 'street', 'zip', 'email', 'phone']
