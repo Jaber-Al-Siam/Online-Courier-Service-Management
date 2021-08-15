f5f94bbba23d46b4df524f99860555b15cad8dba
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-15 17:41:20 +0600

    reformatted views in users

--- users/views.py
+++ users/views.py
@@ -1,38 +1,36 @@
-from django.shortcuts import render, redirect
-from django.views import View
-from users.forms import SignupForm
 from django.contrib import messages
-from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.views import generic
+from users.models import Customer
+from .forms import CustomerCreationForm
 
 
 # Create your views here.
-
 
 def home(request):
     return render(request, 'index.html')
 
 
-def viewOrUpdateProfile(request):
-    return render(request, 'users/profile.html')
+class UserDetailView(generic.DetailView):
+    model = User
+    template_name = 'users/user_detail.html'
 
 
-class UserCreationView(View):
-    form_class = SignupForm
-    template_name = 'registration/signup.html'
+class CustomerDetailView(generic.DetailView):
+    model = Customer
 
-    def get(self, request):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
 
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            form.save()
-            # username = form.cleaned_data.get('username')
-            # raw_password = form.cleaned_data.get('password1')
-            # user = authenticate(username=username, password=raw_password)
-            messages.success(request, 'Your account created successfully, please login')
-            return redirect('login_page')
-        else:
-            form = self.form_class(request.POST)
-            return render(request, self.template_name, {'form': form})
+class CustomerCreationView(generic.edit.CreateView):
+    form_class = CustomerCreationForm
+    template_name = 'users/customer_form.html'
+
+
+class CustomerUpdateView(generic.edit.UpdateView):
+    form_class = CustomerCreationForm
+    template_name = 'users/customer_form.html'
+
+
+class CustomerDeleteView(generic.edit.DeleteView):
+    form_class = CustomerCreationForm
+    template_name = 'users/customer_form.html'
