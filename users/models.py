d7fb4ab8c99e8082669b3ff10bae8686ab04e8aa
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-15 17:44:55 +0600

    added Customer model

--- users/models.py
+++ users/models.py
@@ -1,4 +1,11 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
 
 # Create your models here.
+
+class Customer(User):
+    User._meta.get_field('email')._unique = True
+
+    def get_absolute_url(self):
+        return reverse('users:customer_detail', kwargs={'pk': self.pk})
