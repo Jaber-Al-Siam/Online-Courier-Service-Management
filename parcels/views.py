812f1ce00769b4508492014d0515caa02f8172fe
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-08-18 18:23:16 +0600

    modified ListView and CreateView of parcels

--- parcels/views.py
+++ parcels/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Parcel
 
 
@@ -8,7 +11,7 @@ from .models import Parcel
 
 class ParcelListView(ListView):
     def get_queryset(self):
-        return Parcel.objects.all()
+        return Parcel.objects.filter(booked_by=self.request.user)
 
 
 class ParcelDetailView(DetailView):
@@ -19,12 +22,22 @@ class ParcelCreateView(CreateView):
     model = Parcel
     fields = ['type', 'city', 'street', 'zip', 'email', 'phone']
 
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        parcel = form.save(self)
+        parcel.booked_by = request.user
+        parcel.save()
+        messages.success(request, 'Your parcel has been placed successfully')
+        return redirect('parcels:parcels')
+
 
 class ParcelUpdateView(UpdateView):
     model = Parcel
+    template_name = 'parcels/parcel_update.html'
     fields = ['type', 'city', 'street', 'zip', 'email', 'phone']
 
 
 class ParcelDeleteView(DeleteView):
     model = Parcel
     fields = ['type', 'city', 'street', 'zip', 'email', 'phone']
+    # success_url = 'parcels:parcels'
