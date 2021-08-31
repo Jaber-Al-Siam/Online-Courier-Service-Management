d390cbc66eab0a227738e81082246ef21f824a3c
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-09-01 01:17:59 +0600

    added forms and navigation link for cost calculator

--- parcels/forms.py
+++ parcels/forms.py
@@ -3,3 +3,13 @@ from django import forms
 
 class ParcelTrackForm(forms.Form):
     parcel_id = forms.IntegerField(required=True)
+
+
+class CostCalculatorForm(forms.Form):
+    AREA = [
+        ('inside', 'Inside Dhaka'),
+        ('outside', 'Outside Dhaka'),
+    ]
+
+    weight = forms.IntegerField(required=True)
+    area = forms.ChoiceField(choices=AREA)
