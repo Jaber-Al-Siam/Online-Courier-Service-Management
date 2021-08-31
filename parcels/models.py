3f697381ab68ed8c8cf50eec9b04d8e80bbcf303
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-09-01 01:19:01 +0600

    added parcel status choices

--- parcels/models.py
+++ parcels/models.py
@@ -6,6 +6,14 @@ from users.models import Customer
 
 # Create your models here.
 
+STATUS = [
+    ('pending', 'Pending'),
+    ('processing', 'Processing'),
+    ('picked', 'Picked'),
+    ('on_delivery', 'On Delivery'),
+    ('delivered', 'Delivered'),
+]
+
 
 class Parcel(models.Model):
     type = models.CharField(max_length=20)
@@ -14,8 +22,12 @@ class Parcel(models.Model):
     zip = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    status = models.CharField(max_length=100, default='processing')
-    booked_by = models.OneToOneField(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100, choices=STATUS, default='pending')
+    booked_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        print('I am called')
 
     def get_absolute_url(self):
         return reverse('parcels:detail', kwargs={'pk': self.pk})
