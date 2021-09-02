235e64ce274ed8430f13921a68941e9f4cee6699
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-09-03 01:58:18 +0600

    added job models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from users.models import Customer
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    vacancy = models.IntegerField()
+    position = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    salary = models.IntegerField()
+    email = models.EmailField()
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    applicants = [Customer]
