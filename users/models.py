86f05abe40590f8082bb824a07b8e6c5af04d77d
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-09-04 02:41:08 +0600

    added docstring comments

--- users/models.py
+++ users/models.py
@@ -6,10 +6,15 @@ from django.shortcuts import reverse
 # Create your models here.
 
 class Customer(User):
+    """Customer model
+    for creating customer instance in database
+    """
     phone = models.CharField(max_length=20, default=None)
     User._meta.get_field('email')._unique = True
 
-    def get_absolute_url(self):
+    def get_absolute_url(self: User) -> reverse:
+        """function for getting absolute url for individual customer objects
+        """
         return reverse('users:customer_detail', kwargs={'pk': self.pk})
 
     class Meta:
