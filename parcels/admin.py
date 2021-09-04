ebd249fe0517f5e4de47300fc7d51e4b58e7da45
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-09-05 00:36:32 +0600

    added admin inline in parcel admin

--- parcels/admin.py
+++ parcels/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from django.core.mail import send_mail
-from .models import Parcel
+from .models import Parcel, Issue
+
 
 # Register your models here.
+
+class IssueInline(admin.TabularInline):
+    model = Issue
 
 
 @admin.register(Parcel)
 class ParcelAdmin(admin.ModelAdmin):
     list_display = ['id', 'status']
     readonly_fields = ['pickup_address', 'receiver', 'booked_by', ]
+    inlines = [
+        IssueInline,
+    ]
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
