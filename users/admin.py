9b1952fefe4d232c42ee8c6d253e1840ea3ba219
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-09-04 22:18:50 +0600

    added inline fields in parcels admin view

--- users/admin.py
+++ users/admin.py
@@ -1,16 +1,29 @@
 from django.contrib import admin
 
+from parcels.models import Parcel
 from .models import Customer, DeliveryAgent
 
 
 # Register your models here.
 
+class CustomerParcelInline(admin.TabularInline):
+    model = Parcel
+    fk_name = 'booked_by'
+
+
+class DeliveryAgentParcel(admin.TabularInline):
+    model = Parcel
+    fk_name = 'delivery_agent'
+
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['id']
+    list_display = ['email']
     exclude = ['password', ]
     readonly_fields = ['last_login', 'date_joined', ]
+    inlines = [
+        CustomerParcelInline
+    ]
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
@@ -21,6 +34,9 @@ class DeliveryAgentAdmin(admin.ModelAdmin):
     list_display = ['id']
     exclude = ['password', ]
     readonly_fields = ['last_login', 'date_joined', ]
+    inlines = [
+        DeliveryAgentParcel
+    ]
 
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
