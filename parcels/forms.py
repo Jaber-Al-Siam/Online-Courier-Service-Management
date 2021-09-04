2b56d83618a3e08cbff7160fe92d02dff22cc44e
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-09-04 22:20:30 +0600

    added form for Address, Receiver and Parcel

--- parcels/forms.py
+++ parcels/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 
+from parcels.models import Parcel, Address, Receiver
+
 
 class ParcelTrackForm(forms.Form):
-    parcel_id = forms.IntegerField(required=True)
+    parcel_id = forms.IntegerField()
 
 
 class CostCalculatorForm(forms.Form):
@@ -11,5 +13,23 @@ class CostCalculatorForm(forms.Form):
         ('outside', 'Outside Dhaka'),
     ]
 
-    weight = forms.IntegerField(required=True)
+    weight = forms.IntegerField()
     area = forms.ChoiceField(choices=AREA)
+
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+
+class ReceiverForm(forms.ModelForm):
+    class Meta:
+        model = Receiver
+        exclude = ['address']
+
+
+class ParcelForm(forms.ModelForm):
+    class Meta:
+        model = Parcel
+        fields = ['type', ]
