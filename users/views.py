dedcc5fbfba524b2342632827962a3c2a080feeb
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-09-05 18:52:36 +0600

    added documentation comments

--- users/views.py
+++ users/views.py
@@ -9,11 +9,17 @@ from .forms import CustomerCreationForm
 
 # Create your views here.
 
-def home(request) -> render:
+"""List of views of users."""
+
+def home(request):
     """
-    Function for requesting home page view
-    :param request: get request
-    :return: render with index.html
+    Function for requesting home page view.
+
+    :param request: Type of request from user.
+    :type request: request
+
+    :return: Return render view with index.html.
+    :rtype: django.shortcuts.render
     """
     return render(request, 'index.html')
 
@@ -24,14 +30,24 @@ def home(request) -> render:
 
 class UserDetailView(UserPassesTestMixin, generic.DetailView):
     """
-    This class handles UserDetailView requests from User
-    :param model: Model
-    :param template_name: str
+    This class handles all the detail view requests from user.
+
+    :ivar model: Base model of UserDetailView.
+    :vartype model: django.db.models.Model
+
+    :ivar template_name: Name of the template to render UserDetailView.
+    :vartype template_name: str
     """
-    def test_func(self) -> bool:
+
+    def test_func(self):
         """
-        Function for checking access of requesting user
-        :return: bool
+        Function for checking access permission of requesting user to this view.
+
+        :param self: UserPassesTestMixin instance to check if user has access to this view
+        :type self: django.contrib.auth.mixins.UserPassesTestMixin
+
+        :return: return true if user passes test
+        :rtype: bool
         """
         return True
 
@@ -41,66 +57,125 @@ class UserDetailView(UserPassesTestMixin, generic.DetailView):
 
 class CustomerDetailView(UserPassesTestMixin, generic.DetailView):
     """
-    This class handles CustomerDetailView requests from Customer
-    :param model: Model
+    This class handles all the detail view requests from customer.
+
+    :ivar model: Base model of CustomerDetailView.
+    :vartype model: django.db.models.Model
+
+    :ivar template_name: Name of the template to render CustomerDetailView.
+    :vartype template_name: str
     """
-    def test_func(self: UserPassesTestMixin) -> bool:
+
+    def test_func(self):
+        """
+        Function for checking access permission of requesting user to this view.
+
+        :param self: UserPassesTestMixin instance to check if user has access to this view.
+        :type self: django.contrib.auth.mixins.UserPassesTestMixin
+
+        :return: Return true if user passes test.
+        :rtype: bool
+        """
         return True
 
     model = Customer
+    template_name = 'users/customer_detail.html'
 
 
 class CustomerCreationView(generic.edit.CreateView):
     """
+    This class is responsible to handle Customer creation requests.
 
+    :ivar form_class: Form to pass when creating Customer.
+    :vartype form_class: django.forms.Form
+
+    :ivar template_name: Name of the template to render CustomerCreationView.
+    :vartype template_name: str
     """
     form_class = CustomerCreationForm
     template_name = 'users/customer_form.html'
 
 
 class UserUpdateView(UserPassesTestMixin, generic.edit.UpdateView):
-    """"""
+    """
+    This class handles user profile update requests.
 
-    def test_func(self: UserPassesTestMixin) -> bool:
+    :ivar model: Base model of UserUpdateView.
+    :vartype model: django.db.models.Model
+
+    :ivar fields: Name of the fields to allow change from the user.
+    :vartype fields: list
+
+    :ivar template_name: Name of the template to render UserUpdateView.
+    :vartype template_name: str
+    """
+
+    def test_func(self):
         """
+        Function for checking access permission of requesting user to this view.
 
-        :return:
+        :param self: UserPassesTestMixin instance to check if user has access to this view.
+        :type self: django.contrib.auth.mixins.UserPassesTestMixin
+
+        :return: Return true if user passes test.
+        :rtype: bool
         """
         return True
 
     model = User
     fields = ['username', 'first_name', 'last_name', 'email', ]
-    # form_class = CustomerCreationForm
     template_name = 'users/customer_update.html'
 
 
 class CustomerUpdateView(UserPassesTestMixin, generic.edit.UpdateView):
     """
+    This class handles customer profile update requests.
 
+    :ivar model: Base model of CustomerUpdateView.
+    :vartype model: django.db.models.Model
+
+    :ivar fields: Name of the fields to allow change from the customer.
+    :vartype fields: list
+
+    :ivar template_name: Name of the template to render CustomerUpdateView.
+    :vartype template_name: str
     """
+
     def test_func(self):
         """
+        Function for checking access permission of requesting user to this view.
 
-        :return:
+        :param self: UserPassesTestMixin instance to check if user has access to this view.
+        :type self: django.contrib.auth.mixins.UserPassesTestMixin
+
+        :return: Return true if user passes test.
+        :rtype: bool
         """
         return True
 
     model = Customer
     fields = ['first_name', 'last_name', 'phone', ]
-    # form_class = CustomerCreationForm
     template_name = 'users/customer_update.html'
 
 
 class CustomerDeleteView(UserPassesTestMixin, generic.edit.DeleteView):
     """
+    This class handles customer profile delete requests.
 
+    :ivar model: Base model for CustomerDeleteView.
+    :vartype model: django.db.models.Model
     """
+
     def test_func(self):
         """
+        Function for checking access permission of requesting user to this view.
 
-        :return:
+        :param self: UserPassesTestMixin instance to check if user has access to this view.
+        :type self: django.contrib.auth.mixins.UserPassesTestMixin
+
+        :return: Return true if user passes test.
+        :rtype: bool
         """
         return True
 
-    form_class = CustomerCreationForm
-    template_name = 'users/customer_form.html'
+    model = Customer
