5847c088caf7ebb7af6138bcc09c3bf62f4f0912
Jaber Al Siam <jaber.siam@northsouth.edu>
2021-09-14 21:24:03 +0600

    added unit test

--- users/urls.py
+++ users/urls.py
@@ -1,19 +1,19 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from users import views as customer_views
+from users import views as user_views
 
 app_name = 'users'
 
 """Contains all the urls to access users views."""
 
 urlpatterns = [
-    path('', customer_views.home, name='home'),
-    path('signup/', customer_views.CustomerCreationView.as_view(), name='customer_signup'),
-    path('user/<int:pk>/', customer_views.UserDetailView.as_view(), name='user_detail'),
-    path('user/<int:pk>/update', customer_views.UserUpdateView.as_view(), name='user_update'),
-    path('customer/<int:pk>/', customer_views.CustomerDetailView.as_view(), name='customer_detail'),
-    path('customer/<int:pk>/update', customer_views.CustomerUpdateView.as_view(), name='customer_update'),
+    path('', user_views.home, name='home'),
+    path('signup/', user_views.CustomerCreationView.as_view(), name='customer_signup'),
+    path('user/<int:pk>/', user_views.UserDetailView.as_view(), name='user_detail'),
+    path('user/<int:pk>/update', user_views.UserUpdateView.as_view(), name='user_update'),
+    path('customer/<int:pk>/', user_views.CustomerDetailView.as_view(), name='customer_detail'),
+    path('customer/<int:pk>/update', user_views.CustomerUpdateView.as_view(), name='customer_update'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
